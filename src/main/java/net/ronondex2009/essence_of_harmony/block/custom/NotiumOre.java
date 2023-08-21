34d41b3261b35394ed3faa47762be56c962ed9fe
ronondex2009 <ronandex2009@gmail.com>
2023-08-21 11:29:11 -0400

    renamed:    src/main/java/net/ronondex2009/block/ModBlocks.java -> src/main/java/net/ronondex2009/essence_of_harmony/block/ModBlocks.java 	new file:   src/main/java/net/ronondex2009/essence_of_harmony/block/custom/NotiumOre.java 	renamed:    src/main/java/net/ronondex2009/essence_of_harmony.java -> src/main/java/net/ronondex2009/essence_of_harmony/essence_of_harmony.java 	renamed:    src/main/java/net/ronondex2009/item/ModItems.java -> src/main/java/net/ronondex2009/essence_of_harmony/item/ModItems.java 	new file:   src/main/java/net/ronondex2009/essence_of_harmony/sound/ModSounds.java 	modified:   src/main/resources/assets/essence_of_harmony/lang/en_us.json 	new file:   src/main/resources/assets/essence_of_harmony/sounds.json 	new file:   src/main/resources/assets/essence_of_harmony/sounds/notium_sound_damage.ogg

---
+++ src/main/java/net/ronondex2009/essence_of_harmony/block/custom/NotiumOre.java
@@ -0,0 +1,45 @@
+package net.ronondex2009.essence_of_harmony.block.custom;
+
+import java.util.Random;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.network.chat.Component;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.ronondex2009.essence_of_harmony.block.ModBlocks;
+import net.ronondex2009.essence_of_harmony.sound.ModSounds;
+
+public class NotiumOre extends Block {
+
+    public NotiumOre(Properties p_49795_) {
+        super(p_49795_);
+    }
+    
+   public void attack(BlockState blockState, Level level, BlockPos blockPos, Player player) {
+        player.playSound(ModSounds.NOTIUMDAMAGE.get(), 1, 1);
+        level.setBlock(blockPos, ModBlocks.NOTIUM_ORE_DAMAGED.get().getStateForPlacement(null), UPDATE_ALL_IMMEDIATE, UPDATE_ALL);
+        Random random = new Random();
+        for(int i = 0; i<=50; i++){
+            level.addParticle(ParticleTypes.FLAME , 
+                blockPos.getX() + random.nextDouble(2) - 0.5 , 
+                blockPos.getY() + random.nextDouble(2) - 0.5 , 
+                blockPos.getZ()  + random.nextDouble(2) - 0.5 , 
+                random.nextDouble(0.5)-0.25, random.nextDouble(0.5)-0.25, random.nextDouble(0.5)-0.25);
+        }
+        for(int i = 0; i<=5; i++){
+            level.addParticle(ParticleTypes.NOTE , 
+                blockPos.getX() + random.nextDouble(2) - 0.5 , 
+                blockPos.getY() + random.nextDouble(2) - 0.5 , 
+                blockPos.getZ()  + random.nextDouble(2) - 0.5 , 
+                random.nextDouble(4)-2, random.nextDouble(4)-2, random.nextDouble(4)-2);
+        }
+    }
+
+    
+}
